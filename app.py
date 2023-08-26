10ffc0911982f08ebfe873dbc8559b7d6c992102
Rahhul23theace <deepmachine007@gmail.com>
2023-08-26 15:09:53 +0800

    NEw CHANGES

--- app.py
+++ app.py
@@ -105,8 +105,7 @@ def uploaded_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
+    app.run(host='127.0.0.1', port=8000)
 
 
 
